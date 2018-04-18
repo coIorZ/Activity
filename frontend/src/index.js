a661454f3f45f0b67d3eef08bc9522fba419129e
陈玉婷 <chenyuting@chenyutingdeMacBook-Pro.local>
2018-04-18 13:53:22 +0800

    add self create

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import Kar98k from '98k';
 import home from './modules/home';
 import category from './modules/category';
 import auth from './modules/auth';
+import user from'./modules/user';
 
 const app = Kar98k({
   router: 'hash',
@@ -11,4 +12,5 @@ const app = Kar98k({
 app.module(home)
   .module(category)
   .module(auth)
+  .module(user)
   .start('#app');
