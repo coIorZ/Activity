3af42ae0a493dca8cca38e9994ceab0589cece20
龙潇逸 <xiaoyi@longxiaoyideMacBook-Pro.local>
2018-04-21 00:26:56 +0800

    change bg image

--- frontend/src/components/menu.js
+++ frontend/src/components/menu.js
@@ -1,23 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from '98k';
-//import cx from 'classnames';
-//import styles from './menu.scss';
+import cx from 'classnames';
+import styles from './menu.scss';
 import { Link } from 'react-router-dom';
 
 export default class Menu extends Component {
   render() {
     return(
-      <div className='row bg-dark justify-content-center'>
+      <div className={cx('row justify-content-center', styles['background'])}>
         <div className='nav text-uppercase'>
-         <Link className='nav-item nav-link text-white' to='/'>Home</Link>
-          <Link className='nav-item nav-link text-white' to='/c/music'>Music</Link>
-          <Link className='nav-item nav-link text-white' to='/c/lecture'>Lecture</Link>
-          <Link className='nav-item nav-link text-white' to='/c/party'>Party</Link>
-          <Link className='nav-item nav-link text-white' to='/c/movie'>Movie</Link>
-          <Link className='nav-item nav-link text-white' to='/c/exhibition'>Exhibition</Link>
-          <Link className='nav-item nav-link text-white' to='/c/sport'>Sport</Link>
-          <Link className='nav-item nav-link text-white' to='/c/travel'>Travel</Link>
-          <Link className='nav-item nav-link text-white' to='/c/others'>Others</Link>
+         <Link className={cx('nav-item nav-link', styles['item'])} to='/'>Home</Link>
+          <Link className={cx('nav-item nav-link', styles['item'])} to='/c/music'>Music</Link>
+          <Link className={cx('nav-item nav-link', styles['item'])} to='/c/lecture'>Lecture</Link>
+          <Link className={cx('nav-item nav-link', styles['item'])} to='/c/party'>Party</Link>
+          <Link className={cx('nav-item nav-link', styles['item'])} to='/c/movie'>Movie</Link>
+          <Link className={cx('nav-item nav-link', styles['item'])} to='/c/exhibition'>Exhibition</Link>
+          <Link className={cx('nav-item nav-link', styles['item'])} to='/c/sport'>Sport</Link>
+          <Link className={cx('nav-item nav-link', styles['item'])} to='/c/travel'>Travel</Link>
+          <Link className={cx('nav-item nav-link', styles['item'])} to='/c/others'>Others</Link>
         </div>
       </div>
     );
--- frontend/src/modules/home/pages/home.js
+++ frontend/src/modules/home/pages/home.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import { connect } from '98k';
+import React, { Component } from "react";
+import { connect } from "98k";
 
-import Header from '../../../components/header';
-import Menu from '../../../components/menu';
-import Project from '../components/project';
+import Header from "../../../components/header";
+import Menu from "../../../components/menu";
+import Project from "../components/project";
 
 class Home extends Component {
   componentDidMount() {
     this.props.dispatch({
-      type: 'home/fetchHome',
+      type: "home/fetchHome"
     });
   }
 
@@ -16,19 +16,78 @@ class Home extends Component {
     const { activities } = this.props;
 
     return (
-      <div className='container-fluid'>
+      <div className="container-fluid">
         <Header />
         <Menu />
+        <div className="row section section-header">
+          <div
+            className="parallax parallax-image "
+            style={{
+              position: 'relative',
+              backgroundImage: "url(/Activity/assets/time.png)",
+              width: "100vw",
+              height: "85vh",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              opacity:0.8
+            }}
+          >
+            <div className="content text-center" style={{ marginTop: "300px" }}>
+              <p style={{color:'#ffffff',fontSize:'50px'}}>Start!</p>
+            </div>
+            <span style={{
+              position: 'absolute',
+              bottom: 10,
+              left: '50%',
+              transform: 'translateX(-50%)',
+              color: '#ffffff'
+            }} onClick={this.scroll}>
+              <i className="fas fa-chevron-circle-down fa-3x"/>
+            </span>
+          </div>
+        </div>
         {activities ? (
-          <div className=''>
-            <Project label='Music' activities={activities.filter(a => a.category == 'music')} to='/c/music' />
-            <Project label='Lecture' activities={activities.filter(a => a.category == 'lecture')} to='/c/lecture' />
-            <Project label='Party' activities={activities.filter(a => a.category == 'party')} to='/c/party' />
-            <Project label='Movie' activities={activities.filter(a => a.category == 'movie')} to='/c/movie' />
-            <Project label='Exhibition' activities={activities.filter(a => a.category == 'exhibition')} to='/c/exhibition' />
-            <Project label='Sport' activities={activities.filter(a => a.category == 'sport')} to='/c/sport' />
-            <Project label='Travel' activities={activities.filter(a => a.category == 'travel')} to='/c/travel' />
-            <Project label='Others' activities={activities.filter(a => a.category == 'others')} to='/c/others' />
+          <div className="">
+            <Project
+              label="Music"
+              activities={activities.filter(a => a.category == "music")}
+              to="/c/music"
+            />
+            <Project
+              label="Lecture"
+              activities={activities.filter(a => a.category == "lecture")}
+              to="/c/lecture"
+            />
+            <Project
+              label="Party"
+              activities={activities.filter(a => a.category == "party")}
+              to="/c/party"
+            />
+            <Project
+              label="Movie"
+              activities={activities.filter(a => a.category == "movie")}
+              to="/c/movie"
+            />
+            <Project
+              label="Exhibition"
+              activities={activities.filter(a => a.category == "exhibition")}
+              to="/c/exhibition"
+            />
+            <Project
+              label="Sport"
+              activities={activities.filter(a => a.category == "sport")}
+              to="/c/sport"
+            />
+            <Project
+              label="Travel"
+              activities={activities.filter(a => a.category == "travel")}
+              to="/c/travel"
+            />
+            <Project
+              label="Others"
+              activities={activities.filter(a => a.category == "others")}
+              to="/c/others"
+            />
           </div>
         ) : (
           <div>loading...</div>
@@ -37,9 +96,16 @@ class Home extends Component {
     );
   }
 
-  createActivity = () => {
-    this.props.history.push('/create');
+  scroll = () => {
+    window.scrollTo({
+      top:750,
+      behavior: 'smooth',
+    })
   }
+
+  createActivity = () => {
+    this.props.history.push("/create");
+  };
 }
 
 export default connect(state => state.home)(Home);
